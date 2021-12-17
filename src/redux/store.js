7d39263592b813a93c79e8ab5ce75be66927bd86
Phan Tấn Lộc <ginga550504@gmail.com>
2021-12-17 08:38:48 +0700

    updaet dev

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,8 @@ const store = configureStore({
   reducer: rootReduces,
   middleware: getDefaultMiddleware => getDefaultMiddleware({
     serializableCheck: false
-  })
+  }),
+  devTools: process.env.NODE_ENV !== 'production'
 });
 
 export default store;
